242e67697249ca6e53d906bb30c5a60dce2a35c0
Jonathan Braverman <jonathan.braverman@ctstate.edu>
2024-12-20 22:42:31 +0000

    feedback

--- src/main.c
+++ src/main.c
@@ -4,13 +4,20 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
-#define PORT 8080
+#define PORT 8181
+#define BUFFER_SIZE 1024
 
-int main() {
+int main(int argc, char **argv) {
     int sock = 0;
     struct sockaddr_in serv_addr;
-    char *hello = "Hello, Server!!";
-    char buffer[1024] = {0};
+    char *ip = "127.0.0.1";
+    char *hello = "Hello, Server!!!";    
+    char buffer[BUFFER_SIZE] = {0};
+
+    if(argc == 2)
+    {
+        ip = argv[1];
+    }
 
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         printf("\n Socket creation error \n");
@@ -21,7 +28,7 @@ int main() {
     serv_addr.sin_port = htons(PORT);
 
     // Convert IPv4 and IPv6 addresses from text to binary form
-    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+    if (inet_pton(AF_INET, ip, &serv_addr.sin_addr) <= 0) {
         printf("\nInvalid address/ Address not supported \n");
         return -1;
     }
@@ -32,7 +39,17 @@ int main() {
     }
 
     send(sock, hello, strlen(hello), 0);
-    printf("Hello message sent\n");
+    printf("Hello message sent to %s\n",ip);
+    
+    int valread;
+    do{
+        valread = read(sock, buffer, BUFFER_SIZE);
+    }while (valread == 0);
+    if(valread > 0)
+    {
+      printf("Received ack: %s\n", buffer);        
+    }
+
     close(sock);
     return 0;
 }
